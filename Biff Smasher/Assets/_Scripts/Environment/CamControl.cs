feda9f5dbf876bf263c99f125370988faaa5b78b
Anthony <45619854+CobraKing27@users.noreply.github.com>
2019-02-27 21:37:22 +1100

    Parallax Background
    Parallax forest added

---
+++ Biff Smasher/Assets/_Scripts/Environment/CamControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        transform.position += (Vector3.right * horizontal) * Time.deltaTime;
+
+    }
+}
