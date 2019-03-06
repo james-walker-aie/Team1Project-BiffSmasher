f9e3b9f4366f92b149e4fbaa3757905b200ee5f8
CalumLott <45645769+CalumLott@users.noreply.github.com>
2019-03-06 15:36:00 +1100

    Sword & Attack
    Added new sprites for the character when he collects the sword its subsequent animations.

--- Biff Smasher/Assets/_Scripts/Player/CharacterController2D.cs
+++ Biff Smasher/Assets/_Scripts/Player/CharacterController2D.cs
@@ -8,59 +8,113 @@ public class CharacterController2D : MonoBehaviour
     public float speed;
     public float jumpForce;
     private float moveInput;
+    public float checkRadius;
+            
+    private bool isGrounded;        // determine if player is standing on ground.
+    private bool attack;
+    private bool jump;
+    private bool facingRight;     // set default look direction to right.
     private Rigidbody2D rb;
     public SpriteRenderer sr;
-
-    //private bool facingRight = true;   // set default look direction to right.
-    private bool isGrounded;          // determine if player is standing on ground.
     public Transform groundCheck;
-    public float checkRadius;
     public LayerMask whatIsGround;
     public Animator animator;
+    
 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        attack = false;
+        jump = false;
+        facingRight = true;
     }
+
+
+    void Update()
+    {
+        HandleInput();
+    }
+
 
     private void FixedUpdate()
     {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        HandleMovement(horizontal);
 
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround );         // circle collider to check collision with ground.
+        Flip(horizontal);
+        HandleAttacks();
 
-        moveInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        ResetValues();
 
-        animator.SetFloat("Speed", Mathf.Abs(moveInput));
-
-
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround );         // circle collider to check collision with ground.            
     }
 
-    private void Update()
+
+    private void HandleMovement(float horizontal)
     {
-        if(Input.GetKeyDown(KeyCode.Space))                     // player jump.
+        if (!this.animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))  
+        {
+            rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        }
+        else if(animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))  // to stop player from moving when attack is pressed
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+    }
+
+
+    private void HandleAttacks()
+    {
+        if (attack && !this.animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))   
+        {
+            animator.SetTrigger("attack");
+            
+        }
+    }
+
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            attack = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))                     // player jump.
         {
             rb.velocity = Vector2.up * jumpForce;
             animator.SetTrigger("jump");
         }
 
-        if(Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            animator.SetTrigger("punch");
-        }
+    }
 
 
-        if (moveInput > 0)                                    // to change the players looking direction from right to left.
+
+    private void Flip(float horizontal)   // flip direction character is facing when button pressed.
+    {     
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
         {
-            sr.flipX = false;
-        }
-        else if (moveInput < 0)
-        {
-            sr.flipX = true;
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
         }
     }
 
-   
+
+
+    private void ResetValues()  // function used to reset all values of attack, jump etc.
+    {
+        attack = false;
+        jump = false;
+
+    }
+
+
 
 }
