7f83e0622682ed407f65efed346c2146af6edaa6
CalumLott <45645769+CalumLott@users.noreply.github.com>
2019-03-13 09:44:43 +1100

    gitupdate

---
+++ Biff Smasher/Assets/JTest/GotHit.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GotHit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col != null)
+        {
+            if (col.gameObject.tag == "Enemy")
+
+            {
+                Debug.Log("punched " + col.gameObject.tag);
+                if (col.gameObject.GetComponent<EnemyMovement>().health > 0 && col.gameObject.GetComponent<EnemyMovement>().isDead == false)
+                {
+                    Debug.Log("current health " + col.gameObject.GetComponent<EnemyMovement>().health);
+                    col.gameObject.GetComponent<EnemyMovement>().health--;
+                    Debug.Log("hurt enemy " + col.gameObject.GetComponent<EnemyMovement>().health);
+                    // knockback direction
+                    Vector3 hitDir = col.transform.position - this.transform.position;
+                    // hitDir = hitDir.normalized;
+                    //   other.gameObject.GetComponent<EnemyMovement>().Knockback(hitDir);
+
+                }
+
+                if (col.gameObject.GetComponent<EnemyMovement>().health <= 0 && FightZone.wave1 && col.gameObject.GetComponent<EnemyMovement>().isDead == false)
+                {
+                    Debug.Log("destroy enemy");
+                    col.gameObject.GetComponent<EnemyMovement>().isDead = true;
+                    GameManager.fightZone1Enemies--;
+                    Destroy(col.gameObject, .1f);
+
+                }
+
+                if (col.gameObject.GetComponent<EnemyMovement>().health <= 0 && FightZone.wave2 && col.gameObject.GetComponent<EnemyMovement>().isDead == false)
+                {
+                    Debug.Log("destroy enemy");
+                    col.gameObject.GetComponent<EnemyMovement>().isDead = true;
+                    Destroy(col.gameObject, .1f);
+                    GameManager.fightZone2Enemies--;
+
+
+                }
+                if (col.gameObject.GetComponent<EnemyMovement>().health <= 0 && FightZone.wave3 && col.gameObject.GetComponent<EnemyMovement>().isDead == false)
+                {
+                    Debug.Log("destroy enemy");
+                    col.gameObject.GetComponent<EnemyMovement>().isDead = true;
+                    Destroy(col.gameObject, .1f);
+                    GameManager.fightZone3Enemies--;
+
+
+                }
+                if (col.gameObject.GetComponent<EnemyMovement>().health <= 0 && FightZone.wave4 && col.gameObject.GetComponent<EnemyMovement>().isDead == false)
+                {
+                    Debug.Log("destroy enemy");
+                    col.gameObject.GetComponent<EnemyMovement>().isDead = true;
+                    Destroy(col.gameObject, .1f);
+                    GameManager.fightZone4Enemies--;
+
+
+                }
+            }
+
+        }
+    }
+
+
+}
