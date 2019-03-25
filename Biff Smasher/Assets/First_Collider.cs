76d0f92a24a471ce8ce2eef35cfc14b10e780e39
Anthony <45619854+CobraKing27@users.noreply.github.com>
2019-03-25 20:05:24 +1100

    Anthony
    Fixes

---
+++ Biff Smasher/Assets/First_Collider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class First_Collider : MonoBehaviour
+{
+    //This script os to trigger a text box when the player goes backward in the game.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
