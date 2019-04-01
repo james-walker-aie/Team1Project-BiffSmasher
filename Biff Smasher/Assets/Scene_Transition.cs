a95e17568a6003a1b1df6497d943c2987cd6e6cf
Anthony <45619854+CobraKing27@users.noreply.github.com>
2019-04-01 21:18:47 +1100

    Anthony
    Portal Transition

--- Biff Smasher/Assets/Scene_Transition.cs
+++ Biff Smasher/Assets/Scene_Transition.cs
@@ -5,13 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class Scene_Transition : MonoBehaviour
 {
+    [SerializeField] string levelTransit = "LEVEL TO BE LOADED";
+    [SerializeField] GameObject player;
+    [SerializeField] Animator portalTransitAnimation;
+    // pause length between dialogue
+    [SerializeField] float timeBetweenText = 1f;
 
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        
-
 
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(SceneTransitionCo());
+        }
+    }
 
+    public IEnumerator SceneTransitionCo()
+    {
+
+        player.SetActive(false);
+
+        yield return new WaitForSeconds(timeBetweenText);
+
+        portalTransitAnimation.SetTrigger("OpenPortal");
+
+        yield return new WaitForSeconds(timeBetweenText);
+
+        portalTransitAnimation.SetTrigger("PlayerTransition");
+
+        yield return new WaitForSeconds(timeBetweenText);
+
+        SceneManager.LoadScene(levelTransit);
+    }
 }
