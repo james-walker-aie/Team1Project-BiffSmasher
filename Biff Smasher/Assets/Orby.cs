2c19d87a0aa8b028784e92b2c578c10823cec1d6
Anthony <45619854+CobraKing27@users.noreply.github.com>
2019-04-09 21:41:05 +1000

    Anthony
    Orby update

--- Biff Smasher/Assets/Orby.cs
+++ Biff Smasher/Assets/Orby.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Orby : MonoBehaviour
 {
+
+    [SerializeField] Transform Waypoint;
+
+    [SerializeField] float rotateZ = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveToward();
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        gameObject.transform.Rotate(0, 0, rotateZ * Time.deltaTime);
+    }
+
+    void MoveToward()
+    {
+        Waypoint = GameObject.FindGameObjectWithTag("Orby").transform;
+        transform.position = Vector3.MoveTowards(transform.position, Waypoint.transform.position, 1);
     }
 }
