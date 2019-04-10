4e3aa015c7b6a32fc74b0315bf2642e44a2b5354
CalumLott <45645769+CalumLott@users.noreply.github.com>
2019-04-10 13:21:48 +1000

    Orby Follow

---
+++ Biff Smasher/Assets/Follow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 1f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target.position) > 6f) // move if distance from target is greater than 1
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime,0, 0));
+        }
+    }
+}
